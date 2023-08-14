1f10d01cc27219fbd29ee27892c84b3110271b2e
Tomas Van Roose <tomas.vanroose@dermicus.com>
2023-08-14 15:55:03 +0200

    Initial Commit

---
+++ Pizza Everything/Pizza_EverythingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pizza_EverythingApp.swift
+//  Pizza Everything
+//
+//  Created by Tomas Van Roose on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Pizza_EverythingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
